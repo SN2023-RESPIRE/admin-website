b1f6e153ac043844a384b483d30b2ded31084e14
Raoul1808 <raoulthegeek@gmail.com>
2023-03-27 16:23:16 +0200

    Use python-dotenv to get secret key

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
 from db import db
-import models
 
 
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth.db'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
     from auth import auth
     app.register_blueprint(auth)
