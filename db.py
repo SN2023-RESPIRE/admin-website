80171c43eba10b2e582bddf9d57212e348e8ff91
Raoul1808 <raoulthegeek@gmail.com>
2023-03-30 16:46:55 +0200

    Threshold edition page

--- db.py
+++ db.py
@@ -32,6 +32,34 @@ def get_thresholds():
     return thresholds
 
 
+def get_single_threshold(type):
+    if type not in ['voc', 'co2', 'pm1', 'pm2', 'pm10', 'temperature', 'humidity']:
+        return None
+    try:
+        conn = sqlite3.connect(COREPI_DATABASE_PATH)
+    except:
+        return
+    cur = conn.cursor()
+    cur.execute(f'SELECT {type} FROM air_thresholds WHERE id=1;')
+    value = cur.fetchone()[0]
+    max_value = 0
+    step_value = 1
+    if type == 'co2':
+        max_value = 2550
+        step_value = 10
+    elif type == 'voc':
+        max_value = 65535
+    elif type.startswith('pm'):
+        max_value = 511
+    elif type == 'temperature':
+        max_value = 51
+        step_value = 0.2
+    elif type == 'humidity':
+        max_value = 100
+        step_value = 0.5
+    return value, max_value, step_value
+
+
 def get_current_air_data():
     try:
         conn = sqlite3.connect(COREPI_DATABASE_PATH)
--- home.py
+++ home.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
 from flask_login import login_required
 
-from db import get_thresholds, get_current_air_data
+from db import get_thresholds, get_current_air_data, get_single_threshold
 
 home = Blueprint('home', __name__)
 
@@ -12,3 +12,18 @@ def homepage():
     return render_template('pages/index.html',
                            air_data=get_current_air_data(),
                            thresholds=get_thresholds())
+
+
+@home.route('/edit/<type>')
+@login_required
+def edit_threshold(type):
+    thres_data = get_single_threshold(type)
+    if thres_data is None:
+        abort(404)
+    print(thres_data)
+    value, max_value, step_value = thres_data
+    return render_template('pages/set_threshold.html',
+                           type=type,
+                           current_value=value,
+                           max_value=max_value,
+                           step_value=step_value)
