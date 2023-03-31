0b23e7c7975b6568040a73ec17a7940d72055202
Raoul1808 <raoulthegeek@gmail.com>
2023-03-31 08:43:53 +0200

    Update SQLite database with new threshold

--- db.py
+++ db.py
@@ -14,34 +14,7 @@ def dict_factory(cursor, row):
     return d
 
 
-def get_thresholds():
-    try:
-        conn = sqlite3.connect(COREPI_DATABASE_PATH)
-    except:
-        return None
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-    cur.execute('SELECT voc,'
-                'co2,'
-                'pm1,'
-                'pm2,'
-                'pm10,'
-                'temperature,'
-                'humidity FROM air_thresholds WHERE id=1;')
-    thresholds = cur.fetchone()
-    return thresholds
-
-
-def get_single_threshold(type):
-    if type not in ['voc', 'co2', 'pm1', 'pm2', 'pm10', 'temperature', 'humidity']:
-        return None
-    try:
-        conn = sqlite3.connect(COREPI_DATABASE_PATH)
-    except:
-        return
-    cur = conn.cursor()
-    cur.execute(f'SELECT {type} FROM air_thresholds WHERE id=1;')
-    value = cur.fetchone()[0]
+def get_max_and_step_for_type(type):
     max_value = 0
     step_value = 1
     if type == 'co2':
@@ -57,7 +30,63 @@ def get_single_threshold(type):
     elif type == 'humidity':
         max_value = 100
         step_value = 0.5
+    return max_value, step_value
+
+
+
+def get_thresholds():
+    try:
+        conn = sqlite3.connect(COREPI_DATABASE_PATH)
+    except:
+        return None
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    cur.execute('SELECT voc,'
+                'co2,'
+                'pm1,'
+                'pm2,'
+                'pm10,'
+                'temperature,'
+                'humidity FROM air_thresholds WHERE id=1;')
+    thresholds = cur.fetchone()
+    cur.close()
+    conn.close()
+    return thresholds
+
+
+def get_single_threshold(type):
+    if type not in ['voc', 'co2', 'pm1', 'pm2', 'pm10', 'temperature', 'humidity']:
+        return None
+    try:
+        conn = sqlite3.connect(COREPI_DATABASE_PATH)
+    except:
+        return
+    cur = conn.cursor()
+    cur.execute(f'SELECT {type} FROM air_thresholds WHERE id=1;')
+    value = cur.fetchone()[0]
+    cur.close()
+    conn.close()
+    max_value, step_value = get_max_and_step_for_type(type)
     return value, max_value, step_value
+
+
+def set_threshold(type, value):
+    if type not in ['voc', 'co2', 'pm1', 'pm2', 'pm10', 'temperature', 'humidity']:
+        return False
+    max_value, _ = get_max_and_step_for_type(type)
+    if value > max_value:
+        return False
+    try:
+        conn = sqlite3.connect(COREPI_DATABASE_PATH)
+    except:
+        return False
+    cur = conn.cursor()
+    with conn:
+        cur.execute(f'UPDATE air_thresholds SET {type}={value} WHERE id=1;')
+        conn.commit()
+    cur.close()
+    conn.close()
+    return True
 
 
 def get_current_air_data():
@@ -75,6 +104,8 @@ def get_current_air_data():
                 'temperature,'
                 'humidity FROM air_data WHERE id=1;')
     data = cur.fetchone()
+    cur.close()
+    conn.close()
     return data
 
 
--- home.py
+++ home.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request, redirect, url_for
 from flask_login import login_required
 
-from db import get_thresholds, get_current_air_data, get_single_threshold
+from db import get_thresholds, get_current_air_data, get_single_threshold, set_threshold
 
 home = Blueprint('home', __name__)
 
@@ -14,16 +14,24 @@ def homepage():
                            thresholds=get_thresholds())
 
 
-@home.route('/edit/<type>')
+@home.route('/edit/<type>', methods=['GET'])
 @login_required
 def edit_threshold(type):
     thres_data = get_single_threshold(type)
     if thres_data is None:
         abort(404)
-    print(thres_data)
     value, max_value, step_value = thres_data
     return render_template('pages/set_threshold.html',
                            type=type,
                            current_value=value,
                            max_value=max_value,
                            step_value=step_value)
+
+
+@home.route('/edit/<type>', methods=['POST'])
+@login_required
+def edit_threshold_post(type):
+    value = request.form.get('new-threshold')
+    if not set_threshold(type, float(value)):
+        abort(400)
+    return redirect(url_for('home.homepage'))
